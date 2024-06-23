a805290d4af8f8b3bad6df0183207b8cacddc914
Hexakon <hex4nova@gmail.com>
2024-06-23 13:37:30 +0200

    ändrade lite i haskell-svg:n

--- src/lib/icon/Haskell.svelte
+++ src/lib/icon/Haskell.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-	<polyline points="3 6, 7 12"/>
-	<polyline points="3 18, 7 12"/>
+	<polyline points="2 6, 6 12"/>
+	<polyline points="2 18, 6 12"/>
 	<polyline points="8 18, 12 12"/>
 	<polyline points="8 6, 16 18"/>
-	<polyline points="15 10, 21 10"/>
-	<polyline points="18 14, 21 14"/>
+	<polyline points="16 10, 22 10"/>
+	<polyline points="19 14, 22 14"/>
 </svg>
